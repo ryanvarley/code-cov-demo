b230243f26f24421f6fb4c68db70a792f53ec78b
Ryan Varley <ryan@growthintel.com>
2017-08-15 14:50:20 +0100

    Initial commit

---
+++ dataframe.py
@@ -0,0 +1,42 @@
+import csv
+
+
+class DataList(object):
+
+    def __init__(self, data):
+        # Just a list of data for now
+        self.data = data
+
+    def head(self, n=5):
+        return self.data[:n]
+
+    def tail(self, n=5):
+        return self.data[:n]
+
+    def merge(self, df, how='left'):
+        strategies = {
+            'inner': self._inner_merge,
+            'outer': self._outer_merge,
+        }
+
+        try:
+            method = strategies[how]
+        except KeyError:
+            raise ValueError('{} is not a valid strategy.'.format(how))
+
+        return method(df)
+
+    def _inner_merge(self, df):
+        return list(set(self.data).union(set(df)))
+
+    def _outer_merge(self, df):
+        return self.data + df
+
+    def to_csv(self, fp, index=True):
+        with open(fp, 'w') as f:
+            writer = csv.writer(f)
+            for i, row in enumerate(self.data):
+                if index:
+                    writer.writerow([i, row])
+                else:
+                    writer.writerow([row])
---
+++ helpers.py
@@ -0,0 +1,14 @@
+def sum(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def divide(a, b):
+    return a / b
+
+
+def multiply(a, b):
+    return a * b
