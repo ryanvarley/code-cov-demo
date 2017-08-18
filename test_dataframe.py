2f216a30ec1d2fd8adcc11e56c638feb00074ce0
Ryan Varley <ryan@growthintel.com>
2017-08-18 11:26:58 +0100

    Removed head and tail method

--- test_dataframe.py
+++ test_dataframe.py
@@ -8,14 +8,6 @@ def df_fixture():
     return dataframe.DataList(list(range(10)))
 
 
-def test_head(df_fixture):
-    assert df_fixture.head(5) == list(range(5))
-
-
-def test_tail(df_fixture):
-    assert df_fixture.tail(5) == list(range(5, 10))
-
-
 def test_merge(df_fixture):
     assert df_fixture.merge([20, 21, 22]) == (list(range(10)) + [20, 21, 22])
 
